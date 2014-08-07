f7e548532b5eea53aa68d4adba974f8c0ed56d45
adrianclaude <claude.corpuz@gmail.com>
2014-08-07 23:22:56 +0800

    Tomoya Chart Program
    Updated with Printing Functionality

---
+++ TomoyaChartProgram/TomoyaChartProgram/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace TomoyaChartProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LineChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.printDocument = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog = new System.Windows.Forms.PrintPreviewDialog();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.printForm = new Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.LineChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LineChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.LineChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.LineChart.Legends.Add(legend1);
+            this.LineChart.Location = new System.Drawing.Point(13, 12);
+            this.LineChart.Name = "LineChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Temperature";
+            this.LineChart.Series.Add(series1);
+            this.LineChart.Size = new System.Drawing.Size(585, 321);
+            this.LineChart.TabIndex = 0;
+            this.LineChart.Text = "chart1";
+            // 
+            // printPreviewDialog
+            // 
+            this.printPreviewDialog.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog.Enabled = true;
+            this.printPreviewDialog.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog.Icon")));
+            this.printPreviewDialog.Name = "printPreviewDialog";
+            this.printPreviewDialog.Visible = false;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(12, 339);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(586, 23);
+            this.btnPrint.TabIndex = 1;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // printForm
+            // 
+            this.printForm.DocumentName = "document";
+            this.printForm.Form = this;
+            this.printForm.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter;
+            this.printForm.PrinterSettings = ((System.Drawing.Printing.PrinterSettings)(resources.GetObject("printForm.PrinterSettings")));
+            this.printForm.PrintFileName = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(610, 367);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.LineChart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.LineChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart LineChart;
+        private System.Drawing.Printing.PrintDocument printDocument;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog;
+        private System.Windows.Forms.Button btnPrint;
+        private Microsoft.VisualBasic.PowerPacks.Printing.PrintForm printForm;
+    }
+}
+
---
+++ TomoyaChartProgram/TomoyaChartProgram/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace TomoyaChartProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LineChart.Series["Temperature"].ChartType = SeriesChartType.Line;
+
+            FillData();
+        }
+
+        private void FillData()
+        {
+
+            List<int> arrayofvalues = new List<int>();
+            arrayofvalues.Add(10);
+            arrayofvalues.Add(10);
+            arrayofvalues.Add(10);
+            arrayofvalues.Add(50);
+            arrayofvalues.Add(70);
+            arrayofvalues.Add(30);
+            arrayofvalues.Add(20);
+            arrayofvalues.Add(-10);
+            arrayofvalues.Add(-10);
+            arrayofvalues.Add(30);
+            arrayofvalues.Add(30);
+            arrayofvalues.Add(30);
+            arrayofvalues.Add(50);
+            arrayofvalues.Add(50);
+            arrayofvalues.Add(50);
+            arrayofvalues.Add(50);
+            arrayofvalues.Add(50);
+            arrayofvalues.Add(50);
+            arrayofvalues.Add(50);
+            arrayofvalues.Add(50);
+
+            int index = 0, lasthour = 0;
+            foreach (int a in arrayofvalues)
+            {
+                if (index == 0)
+                {
+                    LineChart.Series["Temperature"].Points.AddXY(0, a);
+                }
+                else
+                {
+                    int diff = 0, hour = 0;
+                    if (a > arrayofvalues[index - 1])
+                    {
+                        diff = a - arrayofvalues[index - 1];
+                        hour = diff / 5;
+                    }
+                    else
+                    {
+                        diff = arrayofvalues[index - 1] - a;
+                        hour = diff / 10;
+
+                        if (hour == 0)
+                            hour = 1;
+                    }
+
+                    LineChart.Series["Temperature"].Points.AddXY(lasthour + hour, a);
+                    lasthour += hour;
+                }
+
+                index++;
+            }
+
+            LineChart.Series["Temperature"].XValueMember = "Time";
+            LineChart.Series["Temperature"].YValueMembers = "Degree";
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            btnPrint.Visible = false;
+            printForm.PrintAction = PrintAction.PrintToPreview;
+            printForm.Print();
+            btnPrint.Visible = true;
+        }
+    }
+}
